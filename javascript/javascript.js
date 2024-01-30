6167983658183297c0d0c51649a183570e9e89f4
Sloly <yahoo3511@gmail.com>
2024-01-31 05:11:30 +0800

    首頁設計完成

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,20 @@
-
+    //自動加載區域相關功能
+    // 1自動加載PC背景
+    document.addEventListener("DOMContentLoaded", function() {
+        const imageDirectory = 'image/piegon_img/'; // 相对路径
+        const imageExtension = '.jpg';
+        const areas = document.querySelectorAll('.pc-areagroup .item-area');
+    
+        areas.forEach((area, index) => {
+            let imageNumber = (index + 1).toString().padStart(2, '0');
+            let imageUrl = `${imageDirectory}${imageNumber}${imageExtension}`;
+            console.log("Loading image from:", imageUrl); // 调试输出
+            area.style.backgroundImage = `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.60) 100%), url(${imageUrl})`;
+        });
+    });
+    
+    
+    
     // 您的代码逻辑
     // 協助載入header
     $(document).ready(function(){
@@ -6,7 +22,7 @@
     });
     // 切換pc比賽紀錄
     $(".pc-header-gp-links-gradeGp").click(function() {
-        $(".pc-header-gp-links-gradeGp-items").fadeToggle(); // 切换淡入淡出
+        $(".pc-header-gp-links-gradeGp-items").toggleClass("visible");
     });
     $(".mobile-header-gp-links-gradeGp").click(function() {
         $(".mobile-header-gp-links-gradeGp-items").slideToggle(); // 切换淡入淡出
