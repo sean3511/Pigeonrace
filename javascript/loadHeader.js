792e65cc3db1042e24ace52950c018c8a6131967
Sloly <yahoo3511@gmail.com>
2024-08-22 02:05:43 +0800

    版本文案更新

---
+++ javascript/loadHeader.js
@@ -0,0 +1,9 @@
+function loadHeader() {
+    fetch('header.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading header:', error));
+  }
+  
